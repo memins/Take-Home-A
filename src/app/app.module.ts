aa59593dcbcd3ff6bcb8a9b54b6f66d8a8fb5b09
Emin Sahin <m.emins@yahoo.com>
2023-12-09 22:44:36 +0100

    Add input component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,10 @@ import { AppComponent } from './app.component';
 import { Task1Component } from './task-1/task-1.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { Task2Component } from './task-2/task-2.component';
+import { InputComponent } from './components/input/input.component';
 
 @NgModule({
-  declarations: [AppComponent, Task1Component, Task2Component],
+  declarations: [AppComponent, Task1Component, Task2Component, InputComponent],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
---
+++ src/app/components/input/input.component.ts
@@ -0,0 +1,45 @@
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  forwardRef,
+} from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true,
+    },
+  ],
+})
+export class InputComponent implements ControlValueAccessor {
+  @Input() type = 'text';
+  value: any;
+
+  updateValue(event: Event): void {
+    const value = (event.target as HTMLInputElement).value;
+    this.onChange(value);
+  }
+
+  onChange: any = () => {};
+  onTouched: any = () => {};
+
+  writeValue(value: any): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+}
